b541aaa0bfc7a8455fa28d7cb10c657011690e08
Elizabeth Wolensky <liz.wolensky@gmail.com>
2015-01-25 15:26:23 -0500

    acts_as_taggable view established

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,10 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.find_or_create_by(link: params["article"]["link"])
+    if @article
+      @article.tag_list.add(params["article"]["tag_list"], :parse => true)
+      @article.save
+    end
     TitlesWorker.perform_async(@article.id)
     redirect_to home_url(current_user.id)
   end
